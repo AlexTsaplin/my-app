136fa6abb7629d7d3825927e3d730ad48f73e36b
KH-21 <cabiturient@gmail.com>
2025-02-12 13:26:35 +0200

    my 1th commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TodoForm from "./TodoForm.jsx";
+
+function TodoItem({ todo, onToggle, onDelete }) {
+    return (
+        <li>
+            <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => onToggle(todo.id)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+            <button onClick={() => onDelete(todo.id)}>Видалити</button>
+        </li>
+    );
+}
+
+export default TodoItem;
